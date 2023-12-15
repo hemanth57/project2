16f1ef2e47df9acc0bad7ffa5023fc8e56fe3fb5
EC2 Default User <ec2-user@ip-172-31-19-14.ec2.internal>
2023-12-15 09:34:15 +0000

    created on ec2 using terraform

---
+++ ec2.tf
@@ -0,0 +1,57 @@
+# Public subnet EC2 instance 1
+resource "aws_instance" "project2-web-server-1" {
+  ami             = "ami-0230bd60aa48260c6"
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.project2-ec2-sg.id]
+  subnet_id       = aws_subnet.project2-pub-sub-1.id
+  key_name   = "project2-key"
+
+  tags = {
+    Name = "project2-web-server-1"
+  }
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y 
+sudo systemctl enable nginx
+sudo systemctl start nginx
+EOF
+}
+
+# Public subnet  EC2 instance 2
+resource "aws_instance" "project2-web-server-2" {
+  ami             = "ami-0230bd60aa48260c6"
+  instance_type   = "t3.micro"
+  security_groups = [aws_security_group.project2-ec2-sg.id]
+  subnet_id       = aws_subnet.project2-pub-sub-2.id
+  key_name   = "project2-key"
+
+  tags = {
+    Name = "project2-web-server-2"
+  }
+
+  user_data = <<-EOF
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install nginx1 -y 
+sudo systemctl enable nginx
+sudo systemctl start nginx
+EOF
+}
+
+#EIP
+
+resource "aws_eip" "project2-web-server-1-eip" {
+  vpc = true
+
+  instance                  = aws_instance.project2-web-server-1.id
+  depends_on                = [aws_internet_gateway.project2-igw]
+}
+
+resource "aws_eip" "project2-web-server-2-eip" {
+  vpc = true
+
+  instance                  = aws_instance.project2-web-server-2.id
+  depends_on                = [aws_internet_gateway.project2-igw]
+}
